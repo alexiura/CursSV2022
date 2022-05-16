7a8ab813a739ecd6a04c61e260471f2597e70168
Iura Alexandru <44611300+alexiura@users.noreply.github.com>
2022-05-16 23:56:51 +0300

    Add files via upload
    primul exercitiu de la tema

---
+++ src/components/exercises/exercise1.js
@@ -0,0 +1,19 @@
+const Exercise1 = () => {
+  return (
+    <div className="container">
+      <div className="pot-top"></div>
+      <div className="pot-top-shadow"></div>
+      <div className="pot-bottle"></div>
+      <div className="plant"></div>
+      <div className="flowerPosition">
+        <div className="flower-petal1 flower"></div>
+        <div className="flower-petal2 flower"></div>
+        <div className="flower-petal3 flower"></div>
+        <div className="hflower-base"></div>
+      </div>
+      <div className="etiquette">Exercițiu 1</div>
+    </div>
+  )
+}
+
+export default Exercise1;
